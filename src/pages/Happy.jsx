772c530add6aef2dd24a936bd3c97ad7683393f5
Andrew Chen <andrewchen@andrews-air-9.lan>
2023-09-05 18:38:06 -0700

    Worked on calling API

--- src/pages/Happy.jsx
+++ src/pages/Happy.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+const CLIENT_ID = "ad2fcb1b0c354f51abf0b4462f76f0ef";
+const CLIENT_SECRET = "7231bd716c85421191380487104dff91";
 
 const Happy = () => {
+  const [accessToken, setAccessToken] = useState('');
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    var authParameters = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+        .then(result => result.json())
+        .then(data => setAccessToken(data.access_token))
+  }, [])
+
+  useEffect(() => {
+    if (accessToken) {
+      getPlaylistData()
+    }
+  }, [accessToken])
+
+  async function getPlaylistData() {
+    var authParameters = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }
+    var returnedPlaylistData = await fetch('https://api.spotify.com/v1/playlists/37i9dQZF1DX3rxVfibe1L0?fields=tracks.items.track(album.images,external_urls,name)', authParameters)
+        .then(result => result.json())
+        // .then(data => console.log(data))
+        .then(data => setSongs(data.tracks.items))
+  }
+
+  console.log(songs)
+
+  // console.log("song photo url: " + songs[0].track.album.images[0].url)
+  // console.log("song url: " + songs[0].track.external_urls.spotify)
+  // console.log("song name: " + songs[0].track.name)
+
   return (
-    <div>Happy</div>
+    <div>
+      <h1>Happy Songs</h1>
+    </div>
+
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Home.css'
 
-const CLIENT_ID = "ad2fcb1b0c354f51abf0b4462f76f0ef";
-const CLIENT_SECRET = "7231bd716c85421191380487104dff91";
-
 const Home = () => {
     const navigate = useNavigate()
-    const [inputValue, setInputValue] = useState('')
-    const [accessToken, setAccessToken] = useState('');
-    const [spotifyURL, setSpotifyURL] = useState('');
     const [dropdownMenu, setDropdownMenu] = useState(false);
     const [selectedOption, setSelectedOption] = useState('click me');
-
-    useEffect(() => {
-        var authParameters = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-        }
-        fetch('https://accounts.spotify.com/api/token', authParameters)
-            .then(result => result.json())
-            .then(data => setAccessToken(data.access_token))
-    }, [])
-
-    // async function getArtist() {
-    //     var authParameters = {
-    //         method: 'GET',
-    //         headers: {
-    //             'Authorization': 'Bearer ' + accessToken
-    //         }
-    //     }
-    //     await fetch('https://api.spotify.com/v1/artists/' + inputValue, authParameters)
-    //         .then(result => result.json())
-    //         .then(data => setSpotifyURL(data.external_urls.spotify))
-    // }
-
-    // const handleKeyDown = (event) => {
-    //     if (event.key === 'Enter') {
-    //         getArtist();
-    //     }
-    // }
 
     const handleOptionClick = (option) => {
         setSelectedOption(option);
